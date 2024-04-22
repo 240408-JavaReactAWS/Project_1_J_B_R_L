f5d7cdeb56045ef0d9f77e63d65e433775038340
jeffg2221 <jeffg2221@icloud.com>
2024-04-22 17:59:03 -0400

    Added controllers

---
+++ StreamFlixBackend/src/main/java/com/revature/StreamFlixBackend/controllers/UserController.java
@@ -0,0 +1,10 @@
+package com.revature.StreamFlixBackend.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class UserController {
+
+}

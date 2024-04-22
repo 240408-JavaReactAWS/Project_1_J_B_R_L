1f9c2a883b9f353e2a8cec19118db7c54b1386eb
briandb3 <75955003+briandb3@users.noreply.github.com>
2024-04-22 16:50:55 -0400

    Movie model with many to many relation commented out

--- StreamFlixBackend/src/main/java/com/revature/StreamFlixBackend/models/Movie.java
+++ StreamFlixBackend/src/main/java/com/revature/StreamFlixBackend/models/Movie.java
@@ -1,6 +1,5 @@
-package com.revature.models;
+package com.revature.StreamFlixBackend.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import org.springframework.stereotype.Component;
 

eb373280ba9b4e84de37b0ad9e292bd050634dd8
rdsherk <ryan.sherk@gmail.com>
2024-04-22 16:17:36 -0400

    added spring initializer project

---
+++ StreamFlixBackend/src/main/java/com/revature/StreamFlixBackend/StreamFlixBackendApplication.java
@@ -0,0 +1,13 @@
+package com.revature.StreamFlixBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamFlixBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamFlixBackendApplication.class, args);
+	}
+
+}

a27d16b0fad485ba1eb3863edb64ea0082f0fc09
jeffg2221 <jeffg2221@icloud.com>
2024-04-23 15:11:37 -0400

    fixed controller mapping and added exception handling

---
+++ StreamFlixBackend/src/main/java/com/revature/StreamFlixBackend/exceptions/UserAlreadyExistsException.java
@@ -0,0 +1,12 @@
+package com.revature.StreamFlixBackend.exceptions;
+
+public class UserAlreadyExistsException extends RuntimeException {
+
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+
+    public UserAlreadyExistsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}

0eb2b49f96382b1a1b0a5ddfc6c19ff950a264cb
lggarcia0 <luisgarcia@alumni.unc.edu>
2024-04-23 15:19:49 -0400

    Added resetUserPassword User Story

--- StreamFlixBackend/src/main/java/com/revature/StreamFlixBackend/controllers/UserController.java
+++ StreamFlixBackend/src/main/java/com/revature/StreamFlixBackend/controllers/UserController.java
@@ -1,10 +1,28 @@
 package com.revature.StreamFlixBackend.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.revature.StreamFlixBackend.models.Users;
+import com.revature.StreamFlixBackend.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/movies")
+@RequestMapping("/users")
 public class UserController {
+    UserService userService;
 
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PatchMapping(value = "/users/{id}")
+    public ResponseEntity<Users> resetPassword(@PathVariable int id, @RequestBody Users user) {
+        Users updatedUser = userService.resetUserPassword(id, user);
+        if (updatedUser != null) {
+            return ResponseEntity.ok().body(updatedUser);
+        } else {
+            return ResponseEntity.status(400).build();
+        }
+    }
 }
--- StreamFlixBackend/src/main/java/com/revature/StreamFlixBackend/services/UserService.java
+++ StreamFlixBackend/src/main/java/com/revature/StreamFlixBackend/services/UserService.java
@@ -1,8 +1,11 @@
 package com.revature.StreamFlixBackend.services;
 
+import com.revature.StreamFlixBackend.models.Users;
 import com.revature.StreamFlixBackend.repos.UserDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -11,5 +14,20 @@ public class UserService {
     @Autowired
     public UserService(UserDAO userDAO) {
         this.userDAO = userDAO;
+    }
+
+    //Users can reset their passwords
+    public Users resetUserPassword(int userId, Users user) {
+        Optional<Users> currentUserOpt = userDAO.findById(userId);
+        if (currentUserOpt.isEmpty()){
+            return null;
+        }
+        Users currentUser = currentUserOpt.get();
+        String newPassword = user.getPassword();
+        if (newPassword.isBlank() || newPassword.length() < 4) {
+            return null;
+        }
+        currentUser.setPassword(newPassword);
+        return userDAO.save(currentUser);
     }
 }
